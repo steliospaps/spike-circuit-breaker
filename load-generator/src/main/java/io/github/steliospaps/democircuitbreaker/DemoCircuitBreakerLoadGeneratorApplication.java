e87a00c4eebd920d68d58932d778cbdef49af7ad
stelios <stelios05@gmail.com>
2020-02-25 21:58:04 +0000

    initial loadtester

---
+++ load-generator/src/main/java/io/github/steliospaps/democircuitbreaker/DemoCircuitBreakerLoadGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.democircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCircuitBreakerLoadGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCircuitBreakerLoadGeneratorApplication.class, args);
+	}
+
+}
---
+++ load-generator/src/test/java/io/github/steliospaps/democircuitbreaker/DemoCircuitBreakerLoadGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.democircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCircuitBreakerLoadGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
