2f71f3159783cb4d9b489dd719f50648e8554097
stelios <stelios05@gmail.com>
2020-02-25 19:48:55 +0000

    initial

---
+++ src/main/java/io/github/steliospaps/democircuitbreaker/DemoCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.democircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/steliospaps/democircuitbreaker/DemoCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.democircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
